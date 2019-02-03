b29a5f175ae93eddb16cc277c9eb811601c2500d
Maneth <maneth.kulatunge@mail.mcgill.ca>
2019-02-03 07:23:58 -0500

    Further modification

--- api_mchacks.py
+++ api_mchacks.py
@@ -18,7 +18,4 @@ def authenticate_client():
     return jsonify({'result':'success'})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
-
-
-
+    app.run(host='0.0.0.0', debug= True, port=5000)
